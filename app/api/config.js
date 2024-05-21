6fcc72a002460da5ae2164b09cfa1aceb6b7b535
Mrdeft2231 <116429749+Mrdeft2231@users.noreply.github.com>
2024-05-22 00:39:56 +0300

    add

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://pindie-frontend.mrdeft.nomoredomainswork.ru/api'
+export const BASE_URL = 'https://backendmrdeft.nomoredomainswork.ru/api'
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
